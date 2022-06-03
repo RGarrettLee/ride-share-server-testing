e9d31c3e186258f646cf08e3c4a1a1097e2f0a23
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2022-06-03 09:11:13 -0400

    ajax

--- script.js
+++ script.js
@@ -4,27 +4,27 @@ let btn = document.querySelector('#post');
 
 let backend = 'https://b493-142-185-241-49.ngrok.io';
 
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-myHeaders.append("Access-Control-Allow-Origin", "*")
-
-var raw = JSON.stringify({
-  "start_lat": 22,
-  "start_lon": 33,
-  "end_lat": 55,
-  "end_lon": 66
-});
-
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
+var settings = {
+    "url": backend,
+    "method": "POST",
+    "timeout": 0,
+    "headers": {
+        "Content-Type": "application/json"
+    },
+    "data": JSON.stringify({
+        "start_lat": 22,
+        "start_lon": 33,
+        "end_lat": 55,
+        "end_lon": 66
+    }),
 };
 
+$.ajax(settings).done(function (response) {
+    console.log(response);
+});
+
 btn.addEventListener('click', function() {
-    fetch("https://b493-142-185-241-49.ngrok.io", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
 })
