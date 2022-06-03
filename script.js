0027a07e040329f26e5afc955a2e0b0b52c7d508
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2022-06-03 08:58:53 -0400

    added origin

--- script.js
+++ script.js
@@ -4,59 +4,27 @@ let btn = document.querySelector('#post');
 
 let backend = 'https://b493-142-185-241-49.ngrok.io';
 
-var settings = {
-  "url": "https://b493-142-185-241-49.ngrok.io",
-  "method": "POST",
-  "timeout": 0,
-  "headers": {
-    "Access-Control-Allow-Origin": "*",
-    "Content-Type": "application/xml",
-    "Accept-Language": "*",
-    "Content-Language": "en-US",
-    "X-PINGOTHER": "something"
-  },
-  "data": JSON.stringify({
-    "start_lat": 22,
-    "start_lon": 33,
-    "end_lat": 55,
-    "end_lon": 66
-  }),
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Access-Control-Allow-Origin", "https://rgarrettlee.github.io/webhook-testing/")
+
+var raw = JSON.stringify({
+  "start_lat": 22,
+  "start_lon": 33,
+  "end_lat": 55,
+  "end_lon": 66
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
 };
 
 btn.addEventListener('click', function() {
-    $.ajax(settings).done(function (response) {
-        console.log(response);
-      });
+    fetch("https://b493-142-185-241-49.ngrok.io", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
 })
-
-/*function sendPost() {
-    fetch(backend, {
-        method: 'POST',
-        mode: 'no-cors',
-        body: {
-            'start_lat': 55,
-            'start_lon': 33,
-            'end_lat': 22,
-            'end_lon': 11
-        }
-    })
-}
-
-function xml() {
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', backend);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-            console.log(xhr.status);
-            console.log(xhr.responseText);
-        }
-    }
-
-    let data = '{"start_lat": 22, "start_lon": 11, "end_lat": 55, "end_lon": 77}';
-
-    xhr.send(data);
-}
-
-btn.addEventListener('click', sendPost)*/
