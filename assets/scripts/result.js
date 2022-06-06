0af1a382b4123921593301a1019e71c1ecb5a5a3
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2022-06-06 04:04:31 -0400

    change

--- assets/scripts/result.js
+++ assets/scripts/result.js
@@ -1,6 +1,21 @@
 const urlParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlParams.entries());
 
+let uberDest = document.querySelector('#dest-uber');
+let uberPrice = document.querySelector('#price-uber');
+let lyftDest = document.querySelector('#dest-lyft');
+let lyftPrice = document.querySelector('#price-lyft');
+
 console.log(params);
 
-console.log('testing');
+if (isNaN(params.uberPrice)) {
+    uberPrice.textContent = 'Error Occured, Please Try Again'
+} else {
+    uberPrice.textContent = `$${params.uberPrice}`;
+}
+
+if (isNaN(params.lyftPrice)) {
+    lyftPrice.textContent = 'Error Occured, Please Try Again'
+} else {
+    uberPrice.textContent = `$${params.lyftPrice}`;
+}
