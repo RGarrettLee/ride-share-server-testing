168a61d03e889989668874e81c6205c68253e9e3
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2022-06-06 02:54:25 -0400

    changes

--- assets/script.js
+++ assets/script.js
@@ -163,7 +163,7 @@ let savedAddresses = {
           params[uberPrice] = uberPrice;
           params[lyftPrice] = lyftPrice;
 
-          location.reload(`./results?${params.toString()}`);
+          location.reload(`./result?${params.toString()}`);
 
           console.log(`Uber Price: ${uberPrice} & Lyft Price: ${lyftPrice}`);
       })
