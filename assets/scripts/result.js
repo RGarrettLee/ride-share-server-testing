8850f03291be4e3ef21b0cede8be8ea87bba32b1
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2022-06-06 04:59:41 -0400

    change

--- assets/scripts/result.js
+++ assets/scripts/result.js
@@ -2,12 +2,12 @@ const urlParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlParams.entries());
 
 let uberDest = document.querySelector('#dest-uber');
-let uberPrice = document.querySelector('#price-uber');
+let uber_price = document.querySelector('#price-uber');
 let lyftDest = document.querySelector('#dest-lyft');
-let lyftPrice = document.querySelector('#price-lyft');
+let lyft_price = document.querySelector('#price-lyft');
 
 uberDest.textContent = params.destination;
 lyftDest.textContent = params.destination;
 
-uberPrice = params.uberPrice;
-lyftPrice = params.lyftPrice;
+uber_price = params.uberPrice;
+lyft_price = params.lyftPrice;
